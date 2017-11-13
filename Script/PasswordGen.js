3debd9073404f4e9f6f85e1790b65586d637ecce
Frankie Pereda <fep1023@gmail.com>
2017-11-13 11:04:03 -0500

    Add files to incomplete dev branch

---
+++ Script/PasswordGen.js
@@ -0,0 +1,79 @@
+function RandomNumber(seed,type) {
+    var new_seed = Math.floor(5*Math.sin(2-6*seed)-4*Math.cos(1+2*seed));
+    var output = "";
+    
+    if (type==1) {
+        return GetUC(12*Math.abs(Math.floor(3*Math.sin(1-2*new_seed)-4*Math.cos(10+2*new_seed))));
+    } else if (type==2) {
+        return GetLC(43*Math.abs(Math.floor(7*Math.sin(2-4*seed)-8*Math.cos(11+4*new_seed))));
+    } else {
+        return GetNum(35*Math.abs(Math.floor(11*Math.sin(3-8*new_seed)-16*Math.cos(12+6*new_seed))));
+    }
+}
+
+// Use String.fromCharCode() to get characters from integers.
+// 97+ for lower case, 65+ for upper case, 48+ for numerals.
+
+function GetUC(seedUC) {
+    // 65 to 90
+    var output = 65;
+    seedUC = seedUC%26;
+    output += seedUC;
+    return String.fromCharCode(output).toString();
+}
+
+function GetLC(seedLC) {
+    // 97 to 122
+    var output = 97;
+    seedLC = seedLC%26;
+    output += seedLC;
+    return String.fromCharCode(output).toString();
+}
+
+function GetNum(seedNum) {
+    // 0 to 9
+    var output = 48;
+    seedNum = seedNum%9;
+    output += seedNum;
+    return String.fromCharCode(output).toString();
+}
+
+function GeneratePassword() {
+    var input = document.getElementById("seed");
+    var seed = Math.abs(input.value)+50;
+    
+    if (isNaN(seed)) {
+        setTimeout(alert("Error, invalid input."),5000);
+        return;
+    }
+    
+    var password = "";
+    var new_seed = 0;
+    var type = 0;
+    
+    for (i=0;i<25;i++) {
+        // Password created here
+        new_seed = seed%(i+5);
+        type = (type+new_seed)%3+1;
+        password = password+RandomNumber(new_seed,type);
+    }
+    try {
+    var node = password;
+    document.getElementById("p").textContent = node;
+    }
+    catch (err) {
+        alert("Error: " + err);
+    }
+}
+
+document.getElementById("function").onclick = GeneratePassword;
+
+var text = document.getElementById("seed");
+var bttn = document.getElementById("function");
+text.addEventListener("keyup",function(event) {
+    event.preventDefault();
+    if (event.keyCode==13) {
+        bttn.click();
+        document.getElementById("function").onclick = GeneratePassword;
+    }
+});
---
+++ make.sh
@@ -0,0 +1,3 @@
+rapydscript -b -p -m Password.py > Password.js
+rm -r PyExtension/Password.js
+mv Password.js PyExtension
